ec8dc6ba82e3d6ae5268cd172d34cbe3a5a961b3
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-02-21 13:33:08 +0200

    best example of fetch from back

--- JS_11_HTTP_ajax/JS_11_HTTP_ajax.js
+++ JS_11_HTTP_ajax/JS_11_HTTP_ajax.js
@@ -1,25 +1,25 @@
 'use strict';
 // аотем рисіч
-const weatherList = document.querySelector('weather-list');
-function getWether(city, days) {
-  const BASE_URL = 'http://api.weatherapi.com/v1/';
-  const API_KEY = 'e0cd171b700f43b39c1134610241902';
-  return fetch(
-    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`
-  )
-    .then(resp => {
-      console.log(resp);
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
-      return resp.json();
-    })
-    .then(data => {
-      console.log(data);
-    });
-}
+// const weatherList = document.querySelector('weather-list');
+// function getWether(city, days) {
+//   const BASE_URL = 'http://api.weatherapi.com/v1/';
+//   const API_KEY = 'e0cd171b700f43b39c1134610241902';
+//   return fetch(
+//     `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`
+//   )
+//     .then(resp => {
+//       console.log(resp);
+//       if (!resp.ok) {
+//         throw new Error(resp.statusText);
+//       }
+//       return resp.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//     });
+// }
 
-getWether('Paris', 5);
+// getWether('Paris', 5);
 
 // макс
 // https://jsonplaceholder.typicode.com/
@@ -59,3 +59,34 @@ getWether('Paris', 5);
 //   .catch(err => {
 //     alert('Incorrect request!');
 //   });
+
+const fetchUsersBtn = document.querySelector('.btn');
+const userList = document.querySelector('.user-list');
+
+// 2
+fetchUsersBtn.addEventListener('click', () => {
+  // console.log(fetchUsers());
+  fetchUsers()
+    .then(users => renderUsers(users))
+    .catch(error => console.log('error'));
+});
+
+//1 порядок написання функцій по моїй логіці
+function fetchUsers() {
+  return fetch('https://jsonplaceholder.typicode.com/users').then(responce => {
+    if (!responce.ok) {
+      throw new Error(`Error:${responce.status}`);
+    }
+    return responce.json();
+  });
+}
+// 3
+function renderUsers(users) {
+  console.log(users);
+  const usersHTML = users
+    .map(({ name, username, company: { bs, name: companyName } }) => {
+      return `<li>name:${name} has username:${username}, bs:${bs}, company name: ${companyName}</li>`;
+    })
+    .join('');
+  userList.insertAdjacentHTML('beforeend', usersHTML);
+}
