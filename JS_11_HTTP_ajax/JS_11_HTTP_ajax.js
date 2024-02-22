4d6a16729f671f70e18ca97a3c36b0754fc57200
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-02-22 13:32:09 +0200

    Wether done

--- JS_11_HTTP_ajax/JS_11_HTTP_ajax.js
+++ JS_11_HTTP_ajax/JS_11_HTTP_ajax.js
@@ -1,25 +1,59 @@
 'use strict';
 // аотем рисіч
-// const weatherList = document.querySelector('weather-list');
-// function getWether(city, days) {
-//   const BASE_URL = 'http://api.weatherapi.com/v1/';
-//   const API_KEY = 'e0cd171b700f43b39c1134610241902';
-//   return fetch(
-//     `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`
-//   )
-//     .then(resp => {
-//       console.log(resp);
-//       if (!resp.ok) {
-//         throw new Error(resp.statusText);
-//       }
-//       return resp.json();
-//     })
-//     .then(data => {
-//       console.log(data);
-//     });
-// }
 
-// getWether('Paris', 5);
+const search = document.querySelector('.js-search');
+const list = document.querySelector('.js-list');
+search.addEventListener('submit', onSearch);
+
+function onSearch(evt) {
+  evt.preventDefault();
+  const { query, days } = evt.currentTarget.elements; // деструктуризуємо елементи форми
+  getWether(query.value, days.value) // передаємо значення в fetch
+    .then(data =>
+      list.insertAdjacentHTML(
+        'beforeend',
+        createMarkup(data.forecast.forecastday)
+      )
+    )
+    .catch(err => console.log(err));
+}
+
+function getWether(city, days) {
+  const BASE_URL = 'http://api.weatherapi.com/v1';
+  const API_KEY = 'e0cd171b700f43b39c1134610241902';
+  return fetch(
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&lang=uk`
+  ) //цей return роверне нам обєкт браузерного api, Response. і ми одразу перевіримо щоб ok:true?newError.Далі ми цей Resp парсимо  в json і передаємо далі,
+    .then(resp => {
+      // console.log(resp); //Obj: Response містить данні про запит, (.ok, status, statusText будуть нам корисні)
+      if (!resp.ok) {
+        throw new Error(resp.statusText); //throw примусово кидає нас в блок .catch
+      }
+      return resp.json(); //повертає проміс, потрібен наступний ланцюжок .then
+    });
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        date,
+        day: {
+          avgtemp_c,
+          condition: { text, icon },
+        },
+      }) =>
+        `<li>
+        <img src="${icon}" alt="${text}" />
+        <p>${text}</p>
+        <h2>${date}</h2>
+        <h3>${avgtemp_c}</h3>
+      </li>`
+    )
+    .join('');
+}
+
+// // -----
 
 // макс
 // https://jsonplaceholder.typicode.com/
@@ -60,33 +94,46 @@
 //     alert('Incorrect request!');
 //   });
 
-const fetchUsersBtn = document.querySelector('.btn');
-const userList = document.querySelector('.user-list');
+// ----
 
-// 2
-fetchUsersBtn.addEventListener('click', () => {
-  // console.log(fetchUsers());
-  fetchUsers()
-    .then(users => renderUsers(users))
-    .catch(error => console.log('error'));
-});
+// const fetchUsersBtn = document.querySelector('.btn');
+// const userList = document.querySelector('.user-list');
 
-//1 порядок написання функцій по моїй логіці
-function fetchUsers() {
-  return fetch('https://jsonplaceholder.typicode.com/users').then(responce => {
-    if (!responce.ok) {
-      throw new Error(`Error:${responce.status}`);
-    }
-    return responce.json();
-  });
-}
-// 3
-function renderUsers(users) {
-  console.log(users);
-  const usersHTML = users
-    .map(({ name, username, company: { bs, name: companyName } }) => {
-      return `<li>name:${name} has username:${username}, bs:${bs}, company name: ${companyName}</li>`;
-    })
-    .join('');
-  userList.insertAdjacentHTML('beforeend', usersHTML);
-}
+// // 2
+// fetchUsersBtn.addEventListener('click', () => {
+//   // console.log(fetchUsers());
+//   fetchUsers()
+//     .then(users => renderUsers(users))
+//     .catch(error => console.log('error'));
+// });
+
+// //1 порядок написання функцій по моїй логіці
+// function fetchUsers() {
+//   return fetch('https://jsonplaceholder.typicode.com/users').then(responce => {
+//     if (!responce.ok) {
+//       throw new Error(`Error:${responce.status}`);
+//     }
+//     return responce.json();
+//   });
+// }
+// // 3
+// function renderUsers(users) {
+//   console.log(users);
+//   const usersHTML = users
+//     .map(
+//       ({
+//         name,
+//         username,
+//         company: { bs, name: companyName },
+//         address: {
+//           geo: { lat },
+//         },
+//       }) => {
+//         return `<li>name:${name} has username:${username}, bs:${bs}, company name: ${companyName}, lat:${lat}</li>`;
+//       }
+//     )
+//     .join('');
+//   userList.insertAdjacentHTML('beforeend', usersHTML);
+// }
+
+// // ----
